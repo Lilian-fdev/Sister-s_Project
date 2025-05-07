90705ae853de19ca5502eb254eba9fe13b376be4
somicodes12 <somtochukwue98@gmail.com>
2025-05-07 23:28:44 +0100

    new commit

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+  
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+  
+    const output = `
+      <h3>Form Submission :</h3>
+      <p><strong>Name :</strong> ${firstName} ${lastName}</p>
+      <p><strong>Email :</strong> ${email}</p>
+      <p><strong>Message :</strong> ${message}</p>
+    `;
+  
+    document.getElementById('formOutput').innerHTML = output;
+  });
